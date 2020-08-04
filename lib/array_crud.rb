22c2fe348fd86a058d4e456a60e96b8587a02b42
Alec Jasik <alec_jasik@yahoo.com>
2020-08-04 19:58:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  ["arrays!", "wow", "hello", "hi", "okay"]
+  [4] = "totally"
 end
